8fd07b29c9ed8ee8cfa0d3bdc6cf109e6afa66a8
vic <vgalkin@mirantis.com>
2013-04-25 17:54:19 +0400

    Returns worker check

--- fuelweb_test/integration/test_admin_node.py
+++ fuelweb_test/integration/test_admin_node.py
@@ -2,14 +2,18 @@ import logging
 import xmlrpclib
 from devops.helpers.helpers import wait, tcp_ping, http
 from fuelweb_test.integration.base_test_case import BaseTestCase
+from fuelweb_test.integration.decorators import debug
 from fuelweb_test.settings import CLEAN
 
+logger = logging.getLogger(__name__)
+logwrap = debug(logger)
 
 class TestPuppetMaster(BaseTestCase):
     def setUp(self):
         if CLEAN:
             self.ci().get_empty_state()
 
+    @logwrap
     def test_puppetmaster_alive(self):
         wait(
             lambda: tcp_ping(self.get_admin_node_ip(), 8140),
@@ -24,6 +28,7 @@ class TestPuppetMaster(BaseTestCase):
         logging.debug("Found puppet master processes: %s" % pm_processes)
         self.assertEquals(len(pm_processes), 4)
 
+    @logwrap
     def test_cobbler_alive(self):
         wait(
             lambda: http(host=self.get_admin_node_ip(), url='/cobbler_api',
@@ -35,8 +40,15 @@ class TestPuppetMaster(BaseTestCase):
         # raises an error if something isn't right
         server.login('cobbler', 'cobbler')
 
+    @logwrap
     def test_nailyd_alive(self):
         ps_output = self.remote().execute('ps ax')['stdout']
         naily_processes = filter(lambda x: '/usr/bin/nailyd' in x, ps_output)
         logging.debug("Found naily processes: %s" % naily_processes)
         self.assertEquals(len(naily_processes), 1)
+        naily_processes = filter(lambda x: 'naily worker' in x, ps_output)
+        logging.debug("Found %d naily worker processes: %s" %
+            (len(naily_processes), naily_processes))
+        self.assertEqual(True, len(naily_processes) > 1)
+
+
